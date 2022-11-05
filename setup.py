796e5e2861d7a6d24bf532895235a962b3767122
MR. RIKI X D3M09 <104522915+MR-RIKI@users.noreply.github.com>
2022-11-05 12:50:36 +0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup, Extension
+
+elf_packages = ['elf', 'elf.core']
+
+setup (name = 'elf',
+       version = '0.1',
+       description = 'dumpelf',
+       packages = elf_packages,
+       author = "D3M09 X RIKI"
+       )
+
